5f1538800d7c7bf799c0d3c198aaed2eb15a5825
Khoa Le <kl58277@uga.edu>
2023-10-09 15:36:08 -0400

    temp fixes

--- src/main/java/edu/uga/cs4370/App.java
+++ src/main/java/edu/uga/cs4370/App.java
@@ -1,16 +1,68 @@
 package edu.uga.cs4370;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @RestController
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+
+  public static void main(String[] args) {
+    Connection conn = null;
+    try {
+      conn =
+        DriverManager.getConnection(
+          "jdbc:mysql://localhost:33306/movie?" + "user=root&password=Lekhoa699"
+        );
+      Statement stmt = null;
+      ResultSet rs = null;
+
+      try {
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery("select * from movies;");
+
+        int colCount = rs.getMetaData().getColumnCount();
+
+        while (rs.next()) {
+          for (int i = 1; i <= colCount; i++) System.out.print(
+            rs.getString(i) + "\t"
+          );
+          System.out.println();
+        }
+      } catch (SQLException ex) {
+        // handle any errors
+        System.out.println("SQLException: " + ex.getMessage());
+        System.out.println("SQLState: " + ex.getSQLState());
+        System.out.println("VendorError: " + ex.getErrorCode());
+      } finally {
+        // it is a good idea to release
+        // resources in a finally{} block
+        // in reverse-order of their creation
+        // if they are no-longer needed
+
+        if (rs != null) {
+          try {
+            rs.close();
+          } catch (SQLException sqlEx) {} // ignore
+
+          rs = null;
+        }
+
+        if (stmt != null) {
+          try {
+            stmt.close();
+          } catch (SQLException sqlEx) {} // ignore
+
+          stmt = null;
+        }
+      }
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
     }
+  }
 }
