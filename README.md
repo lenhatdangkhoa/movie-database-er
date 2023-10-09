f2801404538d786b6d3e774022a4273860deb06c
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-10-09 15:15:43 -0400

    fix readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## MovieLens Database
+
 We are using MovieLens's database for movie ratings. The dataset can be found at [Kaggle](https://www.kaggle.com/datasets/shubhammehta21/movie-lens-small-latest-dataset/)\
-By: [Khoa Le](https://github.com/lenhatdangkhoa), [Gage Roney](https://pornhub.com), [Owen Na](https://pornhub.com),  [Steven Tran](https://pornhub.com)
+By: [Khoa Le](https://github.com/lenhatdangkhoa), [Gage Roney](https://github.com/ragegoney2), [Owen Na](https://github.com/owen-na), [Steven Tran](https://github.com/Tran-Steven)
--- src/main/java/edu/uga/cs4370/model/Comment.java
+++ src/main/java/edu/uga/cs4370/model/Comment.java
@@ -6,12 +6,25 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "comments", schema = "moviereview")
 public class Comment {
-
   private long commentID;
   private int Rating;
   private String Comment;
+
+  public Comment(long commentID, int Rating, String Comment){
+    this.commentID = commentID;
+    this.Rating = Rating;
+    this.Comment = Comment;
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public long getCommentID(){
+    return commentID;
+  }
+public void setCommentID
 }
