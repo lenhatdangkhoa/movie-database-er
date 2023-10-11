d7da986c5b938dee2d9fcda65a9e3aae93f77c26
Khoa Le <kl58277@uga.edu>
2023-10-11 01:06:24 -0400

    finished data class to initialize data

--- src/main/java/edu/uga/cs4370/Data.java
+++ src/main/java/edu/uga/cs4370/Data.java
@@ -2,7 +2,6 @@ package edu.uga.cs4370;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
-import java.io.BufferedReader;  
 import java.io.FileReader;  
 import java.io.IOException; 
 import java.sql.Connection;
@@ -13,6 +12,7 @@ import java.sql.Statement;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Data {
@@ -30,7 +30,7 @@ public class Data {
         }
     }
     /**
-     * ONLY USE THIS METHOD ONCE TO INITIALIZE THE DATABASE.
+     * ONLY USE THIS METHOD ONCE TO INITIALIZE THE DATABASE. 
      */
     public void initializeTables() {
         Statement st = null;
@@ -38,14 +38,14 @@ public class Data {
             st = conn.createStatement();
             String query = "CREATE TABLE User(" +
                 "UserID INT UNIQUE NOT NULL AUTO_INCREMENT, " +
-                "username VARCHAR(20), " + 
-                "password VARCHAR(12), " +
-                "dob DATE, " + 
+                "username VARCHAR(25), " + 
+                "password VARCHAR(25), " +
+                "dob VARCHAR(10), " + 
                 "PRIMARY KEY(UserID))";
             st.execute(query);
             query = "CREATE TABLE Comment(" +
             "CommentID INT UNIQUE NOT NULL AUTO_INCREMENT, " +
-            "timestamp TIME, " +
+            "timestamp TEXT, " +
             "comment VARCHAR(150), " +
             "rating FLOAT, " +
             "PRIMARY KEY(CommentID))"
@@ -62,13 +62,13 @@ public class Data {
             query = "CREATE TABLE Movie(" +
             "MovieID INT UNIQUE NOT NULL AUTO_INCREMENT, " +
             "rating FLOAT, " +
-            "movie_name VARCHAR(25), " +
-            "description VARCHAR(150), " +
+            "movie_name VARCHAR(100), " +
+            "description VARCHAR(200), " +
             "PRIMARY KEY(MovieID));";
             st.execute(query);
             query = "CREATE TABLE Genre(" +
             "GenreID INT UNIQUE NOT NULL AUTO_INCREMENT, " +
-            "genre_name VARCHAR(25), " +
+            "genre_name VARCHAR(50), " +
             "PRIMARY KEY(GenreID));";
             st.execute(query);
             query = "CREATE TABLE MovieComment(" +
@@ -102,26 +102,24 @@ public class Data {
         try {
             CSVReader csvReader = new CSVReaderBuilder(new FileReader("./src/dataset/movies.csv")).withSkipLines(1).build();
 
-            //BufferedReader bf = new BufferedReader(new FileReader("./src/dataset/movies.csv"));
             List<String[]> data = csvReader.readAll();
             Set<String> genres = new HashSet<>();
             for (int i = 0; i < 50; i++) {
-                // try {
-                //     String query = "INSERT INTO Movie VALUES("+i + "," + (1 + new Random().nextFloat() * 4) + ",\"" +
-                //     data.get(i)[1] +"\", \"Very cool movie! Should watch!\");" ;
-                //     st = conn.createStatement();
-                //     st.execute(query);
-                // } catch(SQLException sqle) {
-                //     // handle any errors
-                //     System.out.println("SQLException: " + sqle.getMessage());
-                //     System.out.println("SQLState: " + sqle.getSQLState());
-                //     System.out.println("VendorError: " + sqle.getErrorCode());
-                // }
+                try {
+                    String query = "INSERT INTO Movie VALUES("+i + "," + (1 + new Random().nextFloat() * 4) + ",\"" +
+                    data.get(i)[1] +"\", \"Very cool movie! Should watch!\");" ;
+                    st = conn.createStatement();
+                    st.execute(query);
+                } catch(SQLException sqle) {
+                    // handle any errors
+                    System.out.println("SQLException: " + sqle.getMessage());
+                    System.out.println("SQLState: " + sqle.getSQLState());
+                    System.out.println("VendorError: " + sqle.getErrorCode());
+                }
                 for (String genre : data.get(i)[2].split("\\|")) {
                     genres.add(genre);
                 }
             }
-            System.out.println(genres);
             for (String genre : genres) {
                 String query = "INSERT INTO Genre(genre_name) VALUES(\"" + genre+"\");";
                 try {
@@ -134,13 +132,30 @@ public class Data {
                     System.out.println("VendorError: " + sqle.getErrorCode());
                 }
             }
+            List<String> list = new ArrayList<>(genres);
+            for (int i = 0; i < 50; i++) {
+                for (String genre : data.get(i)[2].split("\\|")) {
+                    String query = "INSERT INTO MovieGenre VALUES("+(i + list.indexOf(genre)) + "," +
+                    i + ","+list.indexOf(genre)+");";
+                    try {
+                    st = conn.createStatement();
+                    st.execute(query);
+                } catch(SQLException sqle) {
+                    // handle any errors
+                    System.out.println("SQLException: " + sqle.getMessage());
+                    System.out.println("SQLState: " + sqle.getSQLState());
+                    System.out.println("VendorError: " + sqle.getErrorCode());
+                }
+                }
+            }
+            
         } catch (IOException ioe) {
             System.out.println(ioe);
         }
     }
     public static void main(String[] args) {
         Data data = new Data();
-        //data.initializeData();
+        data.initializeTables();
         data.initializeData();
 
     }
